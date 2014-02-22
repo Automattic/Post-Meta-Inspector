05ace68514ef1f517f75991729e5314322b58d6f
Risto Niinemets <risto.niinemets@gmail.com>
2014-02-22 12:47:25 +0200

    ajax edit and add metas

--- post-meta-inspector.php
+++ post-meta-inspector.php
@@ -34,6 +34,7 @@ class Post_Meta_Inspector
 
 		add_action( 'init', array( self::$instance, 'action_init') );
 		add_action( 'add_meta_boxes', array( self::$instance, 'action_add_meta_boxes' ) );
+		add_action( 'wp_ajax_pmi_update_post_meta', array( self::$instance, 'action_pmi_update_post_meta' ) );
 	}
 
 	/**
@@ -53,6 +54,23 @@ class Post_Meta_Inspector
 			return;
 
 		add_meta_box( 'post-meta-inspector', __( 'Post Meta Inspector', 'post-meta-inspector' ), array( self::$instance, 'post_meta_inspector' ), get_post_type() );
+	}
+
+	/**
+	 * Edit post meta via AJAX
+	 */
+	public function action_pmi_update_post_meta() {
+		extract( $_POST );
+
+		if( isset( $pmi_meta_name ) && isset( $pmi_meta_value ) && isset( $post_id ) ) {
+			if( $pmi_meta_name == '' ) return;
+
+			update_post_meta( $post_id, $pmi_meta_name, $pmi_meta_value );
+
+			var_export( $pmi_meta_value );
+		}
+
+		exit;
 	}
 
 	public function post_meta_inspector() {
@@ -76,10 +94,13 @@ class Post_Meta_Inspector
 			#post-meta-inspector code {
 				word-wrap: break-word;
 			}
+			#post-meta-inspector input {
+				max-width: 100%;
+			}
 		</style>
 
 		<?php $custom_fields = get_post_meta( get_the_ID() ); ?>
-		<table>
+		<table id="pmi-table">
 			<thead>
 				<tr>
 					<th class="key-column"><?php _e( 'Key', 'post-meta-inspector' ); ?></th>
@@ -98,18 +119,56 @@ class Post_Meta_Inspector
 			?>
 			<tr>
 				<td class="key-column"><?php echo esc_html( $key ); ?></td>
-				<td class="value-column"><?php if( $toggled ) echo $toggle_el; ?><code <?php if( $toggled ) echo ' style="display: none;"'; ?>><?php echo esc_html( $value ); ?></code></td>
+				<td class="value-column"><?php if( $toggled ) echo $toggle_el; ?><code <?php if( $toggled ) echo ' style="display: none;"'; ?> class="pmi_code"><?php echo esc_html( $value ); ?></code></td>
 			</tr>
 			<?php endforeach; ?>
 		<?php endforeach; ?>
+				<tr class="pmi-add-row">
+					<td class="key-column"><input type="text" name="pmi_meta_name"></td>
+					<td class="value-column"><input type="text" name="pmi_meta_value"> <button class="button pmi_submit"><?php _e( 'Add new', 'post-meta-inspector' ) ?></button></td>
+				</tr>
 			</tbody>
 		</table>
 		<script>
-		jQuery(document).ready(function() {
-			jQuery('.pmi_toggle').click( function(e){
-				jQuery('+ code', this).show();
-				jQuery(this).hide();
-			});
+		jQuery(document).ready(function($) {
+			$( '#pmi-table' )
+				.on( 'click', '.pmi_toggle', function(e) {
+					$( '+ code', this ).show();
+					$( this ).hide();
+				})
+				.on( 'click', '.pmi_submit', function(e) {
+					e.preventDefault();
+
+					var post_id	= $( '#post_ID' ).val(),
+						$_pmi_meta_name = $( '#pmi-table' ).find( '.pmi-add-row .key-column input' ),
+						$_pmi_meta_value = $( '#pmi-table' ).find( '.pmi-add-row .value-column input' );
+
+					$.post( "<?php echo admin_url( 'admin-ajax.php' ) ?>", { 'post_id' : post_id, 'pmi_meta_name' : $_pmi_meta_name.val(), 'pmi_meta_value' : $_pmi_meta_value.val(), 'action' : 'pmi_update_post_meta' }, function(response) {
+						if( response == '' ) return;
+
+						$( '#pmi-table .pmi-add-row' ).before( '<tr><td class="key-column">'+ $_pmi_meta_name.val() +'</td><td class="value-column"><code>'+ response +'</code></td></tr>' )
+
+						$_pmi_meta_name.val('');
+						$_pmi_meta_value.val('');
+					});
+				})
+				.on( 'click', '.pmi_code', function(e) {
+					e.preventDefault();
+
+					var new_value = prompt( "<?php _e( 'Enter new value', 'post-meta-inspector' ) ?>" );
+
+					if( new_value != null ) {
+						var post_id	= $( '#post_ID' ).val(),
+							$_this = $( this );
+							$_edit_meta_name = $( this ).closest( 'tr' ).find( '.key-column' );
+
+						$.post( "<?php echo admin_url( 'admin-ajax.php' ) ?>", { 'post_id' : post_id, 'pmi_meta_name' : $_edit_meta_name.text(), 'pmi_meta_value' : new_value, 'action' : 'pmi_update_post_meta' }, function(response) {
+							if( response == '' ) return;
+
+							$_this.html( response );
+						});
+					}
+				});
 		});
 		</script>
 		<?php
